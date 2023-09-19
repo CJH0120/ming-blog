9940905e231d6f4ba6fce6099043318d2e93eb3c
jonghyun <jonghyun@jonghyunui-MacBookPro.local>
2023-09-19 15:39:18 +0900

    gooogle

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -8,6 +8,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="kr">
       <head>
         <meta name="naver-site-verification" content="e489594cb137693445d37999885009aa3104e7bd" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MZMWJF2G');
+            `,
+          }}
+        />
       </head>
       <body className="w-full h-full min-h-screen ">
         <Analytics />
